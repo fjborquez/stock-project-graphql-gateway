3efcaddd0adbe887ee6fccdfc879c0699da43ccb
Francisco Borquez <fborquez@outlook.com>
2023-02-25 17:10:03 -0300

    serverless configuration

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from '@apollo/server/standalone';
 import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway";
+import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
 
 const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
-       { name: "news", url: "https://n5etgb7mr5.execute-api.us-east-1.amazonaws.com/graphql" }
-       // List of federation-capable GraphQL endpoints...
+       { name: "news", url: "https://n5etgb7mr5.execute-api.us-east-1.amazonaws.com/graphql" },
+       { name: "submissions", url: "https://gimdmyklce.execute-api.us-east-1.amazonaws.com/graphql" },
     ],
   }),
 });
 
 const server = new ApolloServer({ gateway });
 
-// Note the top-level await!
-const { url } = await startStandaloneServer(server);
-console.log(`🚀  Server ready at ${url}`);
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server,
+  handlers.createAPIGatewayProxyEventV2RequestHandler(),
+);
